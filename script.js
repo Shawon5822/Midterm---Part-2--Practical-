f9ec78dd8f536d8c813a4e6a04e0200cd1746ac3
Shawon5822 <2020645@iub.edu.bd>
2025-03-22 02:59:30 +0600

    7th

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 function addToCart() {
     alert('Item added to cart!');
 }
+
+// Handle form submission on Contact Page
+function submitForm(event) {
+    event.preventDefault(); // Prevent actual form submission
+    alert('Thank you for contacting us! We will get back to you soon.');
+    document.getElementById('contact-form').reset(); // Reset the form
+}
